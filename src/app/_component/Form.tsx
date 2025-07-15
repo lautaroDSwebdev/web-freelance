efc7dc25af1d6a261ccdac9d26da49ef1597bd45
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-07-15 07:19:30 -0300

    fix: styles form

--- src/app/_component/Form.tsx
+++ src/app/_component/Form.tsx
@@ -2,28 +2,62 @@ import React from "react";
 
 export const Form = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" placeholder="Ingresa tu nombre" />
-      </div>
-      <div>
-        <label htmlFor="apellido">Apellido:</label>
-        <input type="text" id="apellido" name="apellido" placeholder="Ingresa tu apellido" />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="Ingresa tu email" />
-      </div>
-      <div>
-        <label htmlFor="telefono">Teléfono:</label>
-        <input type="tel" id="telefono" name="telefono" placeholder="Ingresa tu número de teléfono" />
-      </div>
-      <div>
-        <label htmlFor="comentario">Comentario:</label>
-        <textarea id="comentario" name="comentario" placeholder="Agrega un comentario" />
-      </div>
-      <button type="submit">Enviar</button>
-    </form>
+    <div className="g-bg-dark p-[5rem]" id="contacto">
+      <article className="max-width mx-auto">
+        <p className="g-styles-minititles g-colour-green text-center p-[1rem">
+          Contacto
+        </p>
+        <h2 className="g-styles-titlesection text-center p-[1rem">
+          ¿Y si hablamos?
+        </h2>
+      </article>
+      <section className="flex justify-center">
+        <form className="p-[3rem]">
+          <div>
+            <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            placeholder="Ingresa tu nombre"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              id="apellido"
+              name="apellido"
+              placeholder="Ingresa tu apellido"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Ingresa tu email"
+            />
+          </div>
+          <div>
+            <input
+              type="tel"
+              id="telefono"
+              name="telefono"
+              placeholder="Ingresa tu número de teléfono"
+            />
+          </div>
+          <div>
+            <textarea
+              id="comentario"
+              name="comentario"
+              placeholder="Agrega un comentario"
+            />
+          </div>
+          <div className="flex justify-end">
+
+          <button className="g-bg-green p-[10px] cursor-pointer text-[#000] font-semibold rounded-[10px]" type="submit">Enviar</button>
+          </div>
+        </form>
+      </section>
+    </div>
   );
 };
--- src/app/_component/Navbar.tsx
+++ src/app/_component/Navbar.tsx
@@ -1,7 +1,8 @@
+"use client"
 import React from "react";
 import { navLinks } from "../_mock/mock";
-
 export const NavBar = () => {
+
   return (
     <div className=" fixed top-0 w-full g-dark-blue h-[3rem] z-[900]">
       <section className="flex justify-between max-width m-auto h-[3rem] items-center">
@@ -9,7 +10,7 @@ export const NavBar = () => {
       <h2>Lautaro Di Salvo</h2>
       <nav className="flex justify-evenly gap-[1rem]">
         {navLinks.map((e) => (
-          <a key={e.id} href={e.href}>
+          <a key={e.id} href={e.href} className={` hover-underline-animation left   `}>
             {e.option}
           </a>
         ))}
