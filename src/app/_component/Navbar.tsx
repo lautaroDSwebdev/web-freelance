da095ffd43a545f546631c02c312639c05c1d5c6
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-07-15 07:24:35 -0300

    feat: nav bar my name redirect home

--- src/app/_component/Navbar.tsx
+++ src/app/_component/Navbar.tsx
@@ -6,8 +6,9 @@ export const NavBar = () => {
   return (
     <div className=" fixed top-0 w-full g-dark-blue h-[3rem] z-[900]">
       <section className="flex justify-between max-width m-auto h-[3rem] items-center">
-
-      <h2>Lautaro Di Salvo</h2>
+      <a href="#home">
+      <h2 className="font-bold">Lautaro Di Salvo</h2>
+      </a>
       <nav className="flex justify-evenly gap-[1rem]">
         {navLinks.map((e) => (
           <a key={e.id} href={e.href} className={` hover-underline-animation left   `}>
