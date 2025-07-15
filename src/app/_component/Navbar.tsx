08865aa9229abc80b25ba8c831c27c78b7e4e2b0
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-07-15 08:21:22 -0300

    fix: navbar mobile

--- src/app/_component/Navbar.tsx
+++ src/app/_component/Navbar.tsx
@@ -1,22 +1,25 @@
-"use client"
+"use client";
 import React from "react";
 import { navLinks } from "../_mock/mock";
 export const NavBar = () => {
-
   return (
-    <div className=" fixed top-0 w-full g-dark-blue h-[3rem] z-[900]">
-      <section className="flex justify-between max-width m-auto h-[3rem] items-center">
-      <a href="#home">
-      <h2 className="font-bold">Lautaro Di Salvo</h2>
-      </a>
-      <nav className="flex justify-evenly gap-[1rem]">
-        {navLinks.map((e) => (
-          <a key={e.id} href={e.href} className={` hover-underline-animation left   `}>
-            {e.option}
-          </a>
-        ))}
-      </nav>
-        </section>
+    <div className=" fixed  bottom-0 xl:top-0 w-full g-dark-blue h-[5rem] xl:h-[3rem] z-[900]">
+      <section className="flex flex-col xl:flex-row justify-between max-width m-auto h-[4rem]  xl:h-[3rem] items-center">
+        <a href="#home">
+          <h2 className="font-bold">Lautaro Di Salvo</h2>
+        </a>
+        <nav className="flex justify-evenly gap-[1rem] p-[14px_0]">
+          {navLinks.map((e) => (
+            <a
+              key={e.id}
+              href={e.href}
+              className={` hover-underline-animation left `}
+            >
+              {e.option}
+            </a>
+          ))}
+        </nav>
+      </section>
     </div>
   );
 };
