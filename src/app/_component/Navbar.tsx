1b2c71cb2d62dcca257ba0330de9c574a3a022fc
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-07-17 11:32:52 -0300

    fix: gradient navbar

--- src/app/_component/Navbar.tsx
+++ src/app/_component/Navbar.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { navLinks } from "../_mock/mock";
 export const NavBar = () => {
   return (
-    <div className=" fixed  bottom-0 md:top-0 w-full g-scroll-animated-color h-[5rem]   z-[900]">
+    <div className=" fixed  bottom-0 md:top-0 w-full g-gradient-blue h-[5rem]   z-[900]">
       <section className="flex flex-col md:flex-row justify-between max-width m-auto h-[5rem]
         items-center">
         <a href="#home">
-          <h2 className="font-bold lg:text-[21px] md:text-[15px] text-[13px]">Lautaro Di Salvo</h2>
+          <h2 className="font-bold lg:text-[21px] md:text-[15px] text-[13px] p-[8px]">Lautaro Di Salvo</h2>
         </a>
         <nav className="flex justify-evenly gap-[1rem] p-[14px_0] ">
           {navLinks.map((e) => (
