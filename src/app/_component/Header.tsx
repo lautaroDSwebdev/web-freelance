5f1c4b566b87673d623a3c4ed6e0c9b0a78e8fca
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-07-17 10:55:04 -0300

    fix: padding footer

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = () => {
   return (
     <div className="g-bg-dark  section-entireheight  p-[3rem_1rem] h-[92dvh]" id="home">
-      <section className="flex flex-col sm:flex-row gap-[21rem] sm:gap-[6rem] lg:gap-[18rem] justify-center max-width m-auto sm:h-[30rem]">
+      <section className="flex flex-col sm:flex-row gap-[21rem] sm:gap-[0rem] lg:gap-[8rem] justify-center max-width m-auto sm:h-[30rem]">
         <article className="h-[10vh]">
           <h3 className="g-styles-titlesection p-[1rem] text-center sm:text-left ">
             Con sede en Argentina
@@ -11,7 +11,7 @@ export const Header = () => {
           <h1 className="g-colour-green g-styles-minititles p-[1rem] text-center sm:text-left">
             Frontend Developer
           </h1>
-          <p className="p-[1rem]  text-center sm:text-left text-[14px] sm:text-[17px]">
+          <p className="p-[1rem]  text-center sm:text-left text-[14px] sm:text-[2.3vw] md:text-[16px]">
             Soy un desarrollador frontend desde que descubrí el mundo del
             desarrollo web, me apasiona crear soluciones digitales que no solo
             funcionen bien, sinó que tambien se vean increibles y esten libres
@@ -33,11 +33,11 @@ export const Header = () => {
         </article>
         <article className="relative mx-auto ">
           <img
-            className="h-auto w-[11rem]   sm:w-[21rem] max-w-[21rem]"
+            className="h-auto w-[11rem]   sm:w-[16rem] lg:w-[21rem] max-w-[21rem]"
             src="./mi-cara.png"
             alt="imagen mia"
           />
-          <div className="absolute  top-[11rem] sm:top-[19rem] flex flex-col sm:flex-row gap-[1rem] sm:gap-[3rem]">
+          <div className="absolute    sm:top-[15rem] lg:top-[18rem] flex flex-col sm:flex-row gap-[1rem] sm:gap-[1rem]">
             <div className=" w-full  flex items-center gap-[10px] g-bg-dark p-[10px] sm:p-[8px] rounded-2xl outline-1 outline-green-600">
              <p className="text-[1.4rem] sm:text-[1.6rem] lg:text-[1.8rem]">3</p> <i className="g-colour-green text-[12px] lg:text-[18px] ">Años de experiencia</i>
             </div>
